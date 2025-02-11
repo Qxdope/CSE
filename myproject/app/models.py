f258779b5cfcc543971c4f01bb74708db0e4adb1
irfantakazumi <136498238+irfantakazumi@users.noreply.github.com>
2025-02-11 12:37:15 +0800

    part 3

--- myproject/app/models.py
+++ myproject/app/models.py
@@ -14,3 +14,22 @@ class Item(models.Model):
     item_description = models.TextField(null=True,default=None, blank=True)
     def __str__(self):
         return str(self.item_id)
+
+#part 3
+
+class Exhibit(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class SavedExhibit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exhibit = models.ForeignKey(Exhibit, on_delete=models.CASCADE)
+    saved_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'exhibit')
